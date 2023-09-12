34dd94d0097e2b7fe24dc8e87216ca89953930c0
chicuongdev2002 <124854803+chicuongdev2002@users.noreply.github.com>
2023-09-12 17:01:04 +0700

    add file bao cao

--- js/baitap.js
+++ js/baitap.js
@@ -93,12 +93,84 @@ console.log(`Diem trung binh cua doi Dolphins la: ${calcAverageD}`);
 console.log(`Diem trung binh cua doi Koalas la: ${calcAverageK}`);
 console.log(`Diem trung binh cua ca 2 doi là: ${(calcAverageD+calcAverageK)/2}`);
 //Coding Challenge #6
-console.log("Coding Challenge #6")
-    // 1.
-const tipC = 0;
-const billC = 0;
-
-function calcTip(billC) {
-    return tipC = billC >= 50 && billC <= 300 ? billC * 0.15 : billC * 0.2;
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+20%.
+Your tasks:
+1. Write a function 'calcTip' that takes any bill value as an input and returns
+the corresponding tip, calculated based on the rules above (you can check out
+the code from first tip calculator challenge if you need to). Use the function
+type you like the most. Test the function using a bill value of 100
+2. And now let's use arrays! So create an array 'bills' containing the test data
+below
+3. Create an array 'tips' containing the tip value for each bill, calculated from
+the function you created before
+4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+Test data: 125, 555 and 44
+*/
+//1.
+console.log("Coding Challenge #6");
+function calcTip(bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
-console.log(`Tien tip cho hoa don: ${billC} la: ${calcTip(125)}`);
+console.log(calcTip(100));
+//2.
+var bills = [125, 555, 44];
+//3.
+var tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+//4.
+var total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(total);  
+   /*
+   Coding Challenge #7
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
+implement the calculations! Remember: BMI = mass / height ** 2 = mass
+/ (height * height) (mass in kg and height in meter)
+Your tasks:
+1. For each of them, create an object with properties for their full name, mass, and
+height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same
+method on both objects). Store the BMI value to a property, and also return it
+from the method
+3. Log to the console who has the higher BMI, together with the full name and the
+respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
+tall.
+*/
+//1.
+console.log("Coding Challenge #7");
+
+function calcBMI(mass, height) {
+    return mass / (height * height)
+}   
+var Mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function() {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+};
+
+var John = {    
+    fullName: 'Iohn Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function() {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+};
+
+if (calcBMI(Mark.mass, Mark.height) > calcBMI(John.mass, John.height)) {    
+    console.log(`${Mark.fullName}'s BMI (${Mark.calcBMI()}) is higher than ${John.fullName}'s (${John.calcBMI()})!`);
+} else if (calcBMI(Mark.mass, Mark.height) < calcBMI(John.mass, John.height)) {    
+    console.log(`${John.fullName}'s BMI (${John.calcBMI()}) is higher than ${Mark.fullName}'s (${Mark.calcBMI()})!`);
+} else {
+    console.log(`Both ${Mark.fullName} and ${John.fullName} have the same BMI (${Mark.calcBMI()})!`);
+}   
+
